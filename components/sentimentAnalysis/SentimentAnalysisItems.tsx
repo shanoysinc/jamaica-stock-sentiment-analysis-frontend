3a9b98907e2ef20bcc4bc7c151fb2929aa0531e8
shanoysinc <shanoy96@gmail.com>
2023-02-03 12:02:40 -0500

    add endpoints

--- components/sentimentAnalysis/SentimentAnalysisItems.tsx
+++ components/sentimentAnalysis/SentimentAnalysisItems.tsx
@@ -1,8 +1,9 @@
-import { Box, Flex, Heading } from "@chakra-ui/react";
+import { Box, Center, Flex, Heading } from "@chakra-ui/react";
 import { SentimentAnalysisCard } from "./SentimentAnalysisCard";
 import React from "react";
 import { useQuery } from "react-query";
 import { getAllCompanyData } from "local-api/companyAnalysis";
+import { Spinner } from "@chakra-ui/react";
 
 export const SentimentAnalysisItems = () => {
   const { data: companyAnalysis, isLoading } = useQuery(
@@ -21,12 +22,21 @@ export const SentimentAnalysisItems = () => {
         Companies Sentiment Analysis
       </Heading>
 
-      <Flex flexWrap={"wrap"} gap={4}>
-        {companyAnalysis &&
-          companyAnalysis.map((data: any, index: number) => (
-            <SentimentAnalysisCard key={`${data.name}-${index}`} data={data} />
-          ))}
-      </Flex>
+      {isLoading ? (
+        <Center>
+          <Spinner size="xl" />
+        </Center>
+      ) : (
+        <Flex flexWrap={"wrap"} gap={4}>
+          {companyAnalysis &&
+            companyAnalysis.map((data: any, index: number) => (
+              <SentimentAnalysisCard
+                key={`${data.name}-${index}`}
+                data={data}
+              />
+            ))}
+        </Flex>
+      )}
     </Box>
   );
 };
